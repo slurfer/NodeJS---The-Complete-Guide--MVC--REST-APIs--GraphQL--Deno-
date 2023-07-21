1b0979686d383e660937e6c025dcfea6802ae40a
Martin Doušek <dousek.martin@gmail.com>
2023-07-21 21:56:32 +0200

    157/

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,11 +14,13 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect("/");
-    })
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+  })
+    .then((result) => console.log(result))
     .catch((err) => console.log(err));
 };
 
