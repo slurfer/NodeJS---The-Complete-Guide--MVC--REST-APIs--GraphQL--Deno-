ef837726b1c967125adac2209e19b2f59a9abdd8
Martin Doušek <dousek.martin@gmail.com>
2023-07-25 14:28:37 +0200

    180

--- util/database.js
+++ util/database.js
@@ -1,9 +1,15 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "root", {
-  dialect: "mysql",
-  host: 'localhost',
-  logging: false
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://root:root@cluster0.fpj93xx.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Connected to database.");
+      callback(client);
+    })
+    .catch((err) => console.log(err));
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
